1dd98899d681a83da0faca7225ce25d9bc2e084f
Takeru Seki <145529438+taker0084@users.noreply.github.com>
2025-08-26 19:36:13 +0900

    Revert "Develop"

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
-  },
+  /* config options here */
 };
 
 module.exports = nextConfig;
